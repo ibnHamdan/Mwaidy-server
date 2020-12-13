74117d0742ece3d45e34a72d8cd266b8bcddc654
ibnHamdan <ibnHamdan@msn.com>
2020-12-14 01:49:53 +0300

    Commented user model cuz migration to SQL

--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  username: String,
-  hash: String,
-  salt: String,
-});
+// const UserSchema = new mongoose.Schema({
+//   username: String,
+//   hash: String,
+//   salt: String,
+// });
 
-mongoose.model("User", UserSchema);
+// mongoose.model("User", UserSchema);
